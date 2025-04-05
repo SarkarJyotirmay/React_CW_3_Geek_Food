7ad173a90b882155b0ecd27a31ddb741b8a0cb84
SarkarJyotirmay <jsarkar0201@gmail.com>
2025-04-05 11:42:54 +0530

    Created nav and hero section

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Nav from "./components/nav_bar/Nav";
+import Hero from "./components/hero/hero";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
