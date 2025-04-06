dc5feaddd4ae7149f244cf23a449a3064f46fc78
SarkarJyotirmay <jsarkar0201@gmail.com>
2025-04-06 20:12:11 +0530

    fixed import of Hero in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Nav from "./components/nav_bar/Nav";
-import Hero from "./components/hero/hero";
+import Hero from "./components/hero/Hero";
 import HouseSection from "./components/house_section/HouseSection";
 import Cards from "./components/cards_section/Cards";
 import Footer from "./components/footer/Footer";
